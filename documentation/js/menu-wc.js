5766ad259800dd970dbb68dba392a4a033b6a3ac
Antonio Antunes <amgas14@gmail.com>
2021-01-29 10:28:55 +0000

    doc commit

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -41,6 +41,88 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                     </ul>
                 </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-toggle="collapse" ${ isNormalMode ?
+                                'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-AppModule-3ff51a80709f290686adefe64c3af0d7"' : 'data-target="#xs-components-links-module-AppModule-3ff51a80709f290686adefe64c3af0d7"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-3ff51a80709f290686adefe64c3af0d7"' :
+                                            'id="xs-components-links-module-AppModule-3ff51a80709f290686adefe64c3af0d7"' }>
+                                            <li class="link">
+                                                <a href="components/AppComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/AuctionCarouselComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AuctionCarouselComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/AuctionFooterComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AuctionFooterComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/AuctionNavbarComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AuctionNavbarComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/AuctionProductCardListComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AuctionProductCardListComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/AuctionSearchComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">AuctionSearchComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
+                            <li class="link">
+                                <a href="modules/AppRoutingModule.html" data-type="entity-link">AppRoutingModule</a>
+                            </li>
+                </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
+                            'data-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/AppPage.html" data-type="entity-link">AppPage</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
+                            : 'data-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
                     <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
